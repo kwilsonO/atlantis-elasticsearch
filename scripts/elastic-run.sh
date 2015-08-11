f5a6835193e5b63fe194f0761564a78863e72a3e
root <root@ip-10-181-123-137.ec2.internal>
2015-08-11 00:30:28 +0000

    quick bug fix

--- scripts/elastic-run.sh
+++ scripts/elastic-run.sh
@@ -1,3 +1,3 @@
 rm -r /root/elk/atlantis-elasticsearch/logs/
 mkdir /root/elk/atlantis-elasticsearch/logs
-elasticsearch-1.7.1/bin/elasticsearch -Des.config=/root/elk/elasticsearch-1.7.1/config-files/m1/elasticsearch.yml > logs/out.log 2> logs/err.log &
+/root/elk/atlantis-elasticsearch/elasticsearch-1.7.1/bin/elasticsearch -Des.config=/root/elk/atlantis-elasticsearch/config-files/m1/elasticsearch.yml > logs/out.log 2> logs/err.log &

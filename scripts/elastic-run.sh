00584b3f6a03dbee5f97ebbd9fe10e7ccb4d34e2
root <root@ip-10-181-123-137.ec2.internal>
2015-08-12 22:19:55 +0000

    added statement in case atlantis log dir doesn't exist already

--- scripts/elastic-run.sh
+++ scripts/elastic-run.sh
@@ -1,4 +1,5 @@
 rm -r /root/elk/atlantis-elasticsearch/logs/
 mkdir /root/elk/atlantis-elasticsearch/logs
+mkdir /var/log/atlantis/
 mkdir /var/log/atlantis/elasticsearch/
 /root/elk/atlantis-elasticsearch/elasticsearch-1.7.1/bin/elasticsearch -Des.config=/root/elk/atlantis-elasticsearch/config-files/m2/elasticsearch.yml > /var/log/atlantis/elasticsearch/out.log 2> /var/log/atlantis/elasticsearch/err.log &

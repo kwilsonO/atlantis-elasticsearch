56e50fc77a090d71e9928f1e4ab257aa150c0fc1
root <root@ip-10-181-123-137.ec2.internal>
2015-08-12 22:18:13 +0000

    changed log location

--- scripts/elastic-run.sh
+++ scripts/elastic-run.sh
@@ -1,3 +1,4 @@
 rm -r /root/elk/atlantis-elasticsearch/logs/
 mkdir /root/elk/atlantis-elasticsearch/logs
-/root/elk/atlantis-elasticsearch/elasticsearch-1.7.1/bin/elasticsearch -Des.config=/root/elk/atlantis-elasticsearch/config-files/m1/elasticsearch.yml > logs/out.log 2> logs/err.log &
+mkdir /var/log/atlantis/elasticsearch/
+/root/elk/atlantis-elasticsearch/elasticsearch-1.7.1/bin/elasticsearch -Des.config=/root/elk/atlantis-elasticsearch/config-files/m2/elasticsearch.yml > /var/log/atlantis/elasticsearch/out.log 2> /var/log/atlantis/elasticsearch/err.log &

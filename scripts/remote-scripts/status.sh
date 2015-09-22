f1a8809beeaacca00d55931f9784c85d39cc8040
Kyle Wilson <kwilson@ooyala.com>
2015-09-21 18:19:36 -0700

    removed time in status

--- scripts/remote-scripts/status.sh
+++ scripts/remote-scripts/status.sh
@@ -10,12 +10,6 @@ source "$CONF_FILE"
 
 myprocid="$(ps -ef | grep "elasticsearch-${ES_VERSION}" | grep -v grep | awk '{print $2}')"
 
-#time running math
-NOW=$(date +%s)
-DIFF=$(($NOW-$ES_START_TIME))
-TIMERUN=$(date -u -d @$DIFF +%H:%M:%S)
-START=$(date -u -d @$ES_START_TIME +%c)
-
 LOCALHOSTNAME=$(uname -n)
 
 echo "${ES_NAME} : ${LOCALHOSTNAME} Status:"
@@ -25,6 +19,6 @@ if [ "${myprocid}" = "" ]; then
 	echo "No elasticsearch process found."
 else
 
-	echo "[${myprocid}] atlantis-elasticsearch running for ${TIMERUN} since ${START}"
+	echo "[${myprocid}] atlantis-elasticsearch running..."
 
 fi
